7811ab5bf5d96f8496f1ee0b1573763943c7879a
ShubhamSingh <shubhamsinghhrx@gmail.com>
2023-10-30 21:55:35 +0530

    crud Operation On Integration Test

---
+++ movies-info-service/src/main/java/com/reactivespring/repository/MovieInfoRepository.java
@@ -0,0 +1,4 @@
+package com.reactivespring.repository;
+
+public interface MovieRepository {
+}
---
+++ movies-info-service/src/test/java/com/reactivespring/repository/MovieInfoRepositoryIntTest.java
@@ -0,0 +1,7 @@
+package com.reactivespring.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MovieInfoRepositoryIntTest {
+
+}
